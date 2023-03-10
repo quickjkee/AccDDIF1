3f91e34f6ac69989d242c675dfea0aa90f7d4af5
quickjkee <askjfasokf@askdjasoi.ee>
2023-03-11 02:33:06 +0300

    init3

--- edm/generate_and_fid.py
+++ edm/generate_and_fid.py
@@ -5,14 +5,14 @@ def run(path_to_model='$INPUT_PATH/edm-ffhq-64x64-uncond-vp.pkl'):
 
     print('===============================================================================================================================')
     print(f'===================GENERATION STARTED using {path_to_model}===================')
-    subprocess.call(f"torchrun --standalone --nproc_per_node=7 generate.py --outdir=fid-tmp --seeds=100000-149999 --subdirs \
+    subprocess.call(f"CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=7 generate.py --outdir=fid-tmp --seeds=100000-149999 --subdirs \
         --network={path_to_model}", shell=True)
 
 
     print('===================FID CALCULATION===================')
     print('====================================')
     print('Final FID')
-    subprocess.call(f"torchrun --standalone --nproc_per_node=7 fid.py calc --images=fid-tmp/final \
+    subprocess.call(f"CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=7 fid.py calc --images=fid-tmp/final \
         --ref=$INPUT_PATH/ffhq-64x64.npz \
         --ref_inc=inception-2015-12-05.pkl", shell=True)
     print('====================================')
@@ -20,7 +20,7 @@ def run(path_to_model='$INPUT_PATH/edm-ffhq-64x64-uncond-vp.pkl'):
     for i in range(40):
         print('====================================')
         print(f'x0_{i} FID')
-        subprocess.call(f"torchrun --standalone --nproc_per_node=7 fid.py calc --images=fid-tmp/x0_{i} \
+        subprocess.call(f"CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=7 fid.py calc --images=fid-tmp/x0_{i} \
             --ref=$INPUT_PATH/ffhq-64x64.npz \
             --ref_inc=inception-2015-12-05.pkl", shell=True)
         print('====================================')
