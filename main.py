779f025263b979197975d029f88efed85365aeee
quickjkee <askjfasokf@askdjasoi.ee>
2023-03-13 17:38:47 +0300

    it

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ dataset = make_dataset(path_to_data, batch_size=b_size)
 
 # Models
 model = DiffModel(net=net,
-                  num_steps=15,
+                  num_steps=5,
                   device=device)
 clip = DirectionLoss(device=device)
 tuner = FineTuner(model=model,
