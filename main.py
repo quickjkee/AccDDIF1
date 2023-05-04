b104c1cbe38fedf6bf5c821329c42509912b2300
quickjkee <askjfasokf@askdjasoi.ee>
2023-05-04 15:00:03 +0300

    ema added

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ dataset = make_dataset(path_to_data, batch_size=b_size)
 
 # Models
 model = DiffModel(net=net,
-                  num_steps=10,
+                  num_steps=15,
                   device=device)
 clip = DirectionLoss(device=device)
 tuner = FineTuner(model=model,
