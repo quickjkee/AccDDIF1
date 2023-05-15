aadfdfcab08384173e16943a20e09aa378fc283f
quickjkee <askjfasokf@askdjasoi.ee>
2023-05-15 23:33:11 +0300

    FID VIEBY

--- edm/generate_and_fid.py
+++ edm/generate_and_fid.py
@@ -5,7 +5,7 @@ import shutil
 def run(path_to_model, path_to_copy, n_steps):
 
     steps = [10, 11, 12, 15, 20]
-    sigmas = [3, 3.5, 4, 4.5, 5]
+    sigmas = [3, 2.5, 2, 1.5, 3.5, 4, 4.5, 5]
     for sigma in sigmas:
         for n_steps in steps:
             print(n_steps)
