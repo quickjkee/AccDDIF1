fff9847c08e8c4a57499c4e5592125c82dc0f279
quickjkee <askjfasokf@askdjasoi.ee>
2023-05-15 20:52:56 +0300

    FID VIEBY

--- edm/generate.py
+++ edm/generate.py
@@ -308,10 +308,10 @@ def main(network_pkl, network_pkl_copy, sigma_max, outdir, subdirs, seeds, class
         # Init samples
         images, x0_images = sampler_fn(net=net, num_steps=10, latents=latents1, class_labels=class_labels,
                                        randn_like=rnd.randn_like, second_ord=False)
-        x_init = x0_images[6].to(device)
+        #x_init = x0_images[6].to(device)
 
-        images, x0_images = sampler_fn(net=copy_net, x_init=x_init, sigma_max=sigma_max, num_steps=10, second_ord=True,
-                                       latents=latents2, class_labels=class_labels, randn_like=rnd.randn_like)
+        #images, x0_images = sampler_fn(net=copy_net, x_init=x_init, sigma_max=sigma_max, num_steps=10, second_ord=True,
+        #                               latents=latents2, class_labels=class_labels, randn_like=rnd.randn_like)
 
         # Save images.
         images_np = (images * 127.5 + 128).clip(0, 255).to(torch.uint8).permute(0, 2, 3, 1).cpu().numpy()
