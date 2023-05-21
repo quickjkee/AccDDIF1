7e3420338d8f190c6d161ebbf62b95622e112d65
quickjkee <askjfasokf@askdjasoi.ee>
2023-05-21 22:44:13 +0300

    i hope the last

--- edm/generate_and_fid.py
+++ edm/generate_and_fid.py
@@ -5,7 +5,7 @@ import shutil
 def run(path_to_model, path_to_copy, n_steps):
 
     steps = [31]
-    sigmas = [2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3]
+    sigmas = [2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 4.0]
     for sigma in sigmas:
         for n_steps in steps:
             print(n_steps)
