66bc0afc2c86dc9196121c255dfdd6f3f663a6d1
quickjkee <askjfasokf@askdjasoi.ee>
2023-05-25 20:20:29 +0300

    i hope the last

--- edm/generate_and_fid.py
+++ edm/generate_and_fid.py
@@ -10,8 +10,8 @@ INPUT_PATH = os.environ['INPUT_PATH']
 def run(path_to_model, path_to_copy, n_steps):
 
     path = f'{INPUT_PATH}/AccDDIF_sota_ffhq/ultramar_exp_estimate/data_cifar/out'
-    steps = [10]
-    sigmas = [0.5]
+    steps = [10, 15, 20, 25, 30, 40]
+    sigmas = [0.5, 0.6, 0.8, 1.0, 1.5]
     for n_steps in steps:
         for sigma in sigmas:
             print(n_steps)
@@ -26,14 +26,6 @@ def run(path_to_model, path_to_copy, n_steps):
             print('====================================')
             print('Final FID')
             subprocess.call(f"CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=8 edm/fid.py calc --images=fid-tmp/final \
-                --ref=$INPUT_PATH/ffhq-64x64.npz \
-                --ref_inc=edm/inception-2015-12-05.pkl", shell=True)
-            print('====================================')
-
-            print('===================FID INIT===================')
-            print('====================================')
-            print('Final FID')
-            subprocess.call(f"CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=8 edm/fid.py calc --images={INPUT_PATH}/AccDDIF_sota_ffhq/ultramar_exp_estimate/data_cifar/out \
                 --ref=$INPUT_PATH/ffhq-64x64.npz \
                 --ref_inc=edm/inception-2015-12-05.pkl", shell=True)
             print('====================================')
