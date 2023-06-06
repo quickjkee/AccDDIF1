a06bcd6bd6e356b85297420e7b2be4bb38b29bac
quickjkee <askjfasokf@askdjasoi.ee>
2023-06-06 18:18:11 +0300

    imagen ya daun

--- edm/generate_and_fid.py
+++ edm/generate_and_fid.py
@@ -11,14 +11,14 @@ def run(path_to_model, path_to_copy, n_steps):
 
     path = f'{INPUT_PATH}/AccDDIF_sota_ffhq/ultramar_exp_estimate/data_cifar/imagenet_4_cd.zip'
     steps = [64]
-    sigmas = [4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2, 5.3, 5.4]
+    sigmas = [4.4, 4.5, 4.6, 4.7, 4.8]
     for n_steps in steps:
         for sigma in sigmas:
             print(n_steps)
             print('===============================================================================================================================')
             print(f'===================GENERATION STARTED using {path_to_model}===================')
             print(f'===================STEPS {n_steps}, SIGMA {sigma}===================')
-            subprocess.call(f"CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=8 edm/generate.py --outdir=fid-tmp --seeds=50000-99999 --subdirs \
+            subprocess.call(f"CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=8 edm/generate.py --outdir=fid-tmp --seeds=0-49999 --subdirs \
                 --network={path_to_model} --network_copy={path_to_copy} --sigma_max={sigma} --steps={n_steps} \
                 --path={path}", shell=True)
 
@@ -26,15 +26,15 @@ def run(path_to_model, path_to_copy, n_steps):
             print('====================================')
             print('Final FID')
             subprocess.call(f"CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=8 edm/fid.py calc --images=fid-tmp/final \
-                --ref=$INPUT_PATH/ffhq-64x64.npz \
+                --ref=$INPUT_PATH/imagenet-64x64.npz \
                 --ref_inc=edm/inception-2015-12-05.pkl", shell=True)
             print('====================================')
 
-            for i in range(10 + 1):
+            for i in [n_steps - 1]:
                 print('====================================')
                 print(f'x0_{i} FID')
                 subprocess.call(f"CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=8 edm/fid.py calc --images=fid-tmp/x0_{i} \
-                    --ref=$INPUT_PATH/ffhq-64x64.npz \
+                    --ref=$INPUT_PATH/imagenet-64x64.npz \
                     --ref_inc=edm/inception-2015-12-05.pkl", shell=True)
                 print('====================================')
             print(
