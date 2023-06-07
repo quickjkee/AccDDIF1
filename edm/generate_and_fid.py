decbfd842c1425a8c7c55ebaaafb7d7b4fd8afcb
quickjkee <askjfasokf@askdjasoi.ee>
2023-06-07 14:50:00 +0300

    imagen ya daun

--- edm/generate_and_fid.py
+++ edm/generate_and_fid.py
@@ -10,8 +10,8 @@ INPUT_PATH = os.environ['INPUT_PATH']
 def run(path_to_model, path_to_copy, n_steps):
 
     path = f'{INPUT_PATH}/AccDDIF_sota_ffhq/ultramar_exp_estimate/data_cifar/imagenet_4_cd.zip'
-    steps = [86]
-    sigmas = [4.9, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2, 6.3, 6.4]
+    steps = [210]
+    sigmas = [5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2, 6.3, 6.4]
     for n_steps in steps:
         for sigma in sigmas:
             print(n_steps)
