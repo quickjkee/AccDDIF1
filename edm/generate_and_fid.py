7c8035487cac8c2322376e2bf16418c29b90ceff
quickjkee <askjfasokf@askdjasoi.ee>
2023-06-24 15:39:38 +0300

    another try

--- edm/generate_and_fid.py
+++ edm/generate_and_fid.py
@@ -18,9 +18,9 @@ print(sys.path)
 
 def run(path_to_model, path_to_copy, n_steps):
 
-    path = f'{INPUT_PATH}/AccDDIF_sota_ffhq/ultramar_exp_estimate/data_cifar/imagenet_4_cd.zip'
+    path = f'{INPUT_PATH}/AccDDIF_sota_ffhq/ultramar_exp_estimate/data_cifar/imagenet_2_cons.zip'
 
-    steps = [226]
+    steps = [206]
     sigmas = [5.5, 7.0, 8.0, 9.0, 8.5]
     for n_steps in steps:
         for sigma in sigmas:
