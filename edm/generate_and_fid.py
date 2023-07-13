75a187806e46c562aa5725d1323f896e80cbabd4
quickjkee <askjfasokf@askdjasoi.ee>
2023-07-13 10:55:16 +0200

    cats try

--- edm/generate_and_fid.py
+++ edm/generate_and_fid.py
@@ -62,7 +62,7 @@ def run2(edm_path, cons_path, n_steps):
     path_to_ref = f'{INPUT_PATH}/AccDDIF_sota_ffhq/ultramar_exp_estimate/data_cifar/VIRTUAL_lsun_cat256.npz'
 
     steps = [4, 6, 8, 12, 15, 18, 21, 25, 27, 30, 32, 35, 38, 40]
-    sigmas = [6.0]
+    sigmas = [2.0]
     for n_steps in steps:
         for sigma in sigmas:
             print(n_steps)
